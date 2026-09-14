1c8fcde89fda501bba6040e27652ed8a42f657b3
Perfected1 <chikennamadim65@gmail.com>
2026-09-14 11:16:02 +0100

    create first todo task

---
+++ main.py
@@ -0,0 +1,19 @@
+todo = [
+    {
+        "id":1,
+        "title": "Learn python",
+        "completed": False
+    },
+    {
+        "id":2,
+        "title": "Visit Ibadan",
+        "completed": False
+    },
+    {
+        "id":3,
+        "title": "See Movie",
+        "completed": False
+    }
+]
+
+print(todo)
