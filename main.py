8155343cd3bf767d7337cb896c8b9dd3264768db
Perfected1 <chikennamadim65@gmail.com>
2026-09-15 10:03:03 +0100

    Add new todo to list

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
-todo = [
+todos = [
     {
         "id":1,
         "title": "Learn python",
-        "completed": False
-    },
-    {
-        "id":2,
-        "title": "Visit Ibadan",
-        "completed": False
-    },
-    {
-        "id":3,
-        "title": "See Movie",
         "completed": False
     }
 ]
 
 title = input("What do you want to do? ")
+
+new_todo = {
+    "id":2,
+    "title": title,
+    "completed": False
+}
+todos.append(new_todo)
 
 print(title)
