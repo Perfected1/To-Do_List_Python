e2ad8e703f78ff34b5a2f3f3c4a90fd0839b623a
Perfected1 <chikennamadim65@gmail.com>
2026-09-15 11:12:20 +0100

    Add edit todo functionality

--- main.py
+++ main.py
@@ -3,13 +3,21 @@ todos = [
         "id":1,
         "title": "Learn python",
         "completed": False
+    },
+    {
+        "id":2,
+        "title": "Visit Uyo",
+        "completed": True
     }
 ]
+
+
+# Add Task
 
 title = input("What do you want to do? ")
 
 new_todo = {
-    "id":2,
+    "id":3,
     "title": title,
     "completed": False
 }
@@ -21,6 +29,9 @@ for todo in todos:
     else:
         status = "Not Done"
     print(todo["id"], todo["title"], "-", status)
+
+
+# Mark Task
 
 found = False
 
@@ -36,4 +47,19 @@ for todo in todos:
 if not found:
         print("Task not Available!")
 
-print(todo["id"], todo["title"], "-", status)
+# print(todo["id"], todo["title"], "-", status)
+
+
+# Edit Existing Task
+found = False
+
+todo_id = int(input("Enter ID of Task to be Edited: "))
+for todo in todos:
+    if todo["id"] == todo_id:
+            
+        new_title = input("Enter New Title: ")
+        todo["title"] = new_title
+        found = True
+        print("Task updated successfully!")
+if not found:
+    print("Task not Available!")
